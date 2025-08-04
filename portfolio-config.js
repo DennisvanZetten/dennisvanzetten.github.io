a4ec055d62b5d0d806cefd0761aaf16ee235a46d
Dennis.vanzetten <dennis.vanzetten@iodigital.com>
2025-08-04 21:52:04 +0200

    update

---
+++ portfolio-config.js
@@ -0,0 +1,54 @@
+// portfolio-config.js
+// Fotografie portfolio configuratie - Dennis van Zetten
+
+const PORTFOLIO_CONFIG = {
+  landschappen: {
+    title: "Landschappen",
+    description: "Nederlandse natuur en landschapsfotografie",
+    // Zet je landschapsfoto's in images/portfolio/landschappen/
+    images: [
+      {
+        filename: "posbank-zonsopkomst.jpg",
+        title: "Posbank bij zonsopgang",
+        description:
+          "Klassiek Nederlands landschap met heide in het ochtendlicht",
+      },
+    ],
+  },
+  portfolio2025: {
+    title: "Portfolio 2025",
+    description: "Mijn beste werk van het nieuwe jaar",
+    // Zet je beste 2025 foto's in images/portfolio/portfolio2025/
+    images: [],
+  },
+  macro: {
+    title: "Macro Fotografie",
+    description: "Macro shots van insecten en bloemen",
+    images: [
+      {
+        filename: "paddestoelen-cluster.jpg",
+        title: "Paddestoelen Hongarije",
+        description:
+          "Paddestoelen in een cluster, vastgelegd in het bos van Hongarije",
+      },
+      {
+        filename: "paddestoel-zwartwit.jpg",
+        title: "Paddestoel zwart wit, Hongarije",
+        description:
+          "Paddestoelen zwart wit, vastgelegd in het bos van Hongarije",
+      },
+      {
+        filename: "paddestoel-tegenlicht.jpg",
+        title: "Paddestoel met tegenlicht, Hongarije",
+        description:
+          "Paddestoel prachtig verlicht, vastgelegd in het bos van Hongarije",
+      },
+    ],
+  },
+  zwartwit: {
+    title: "Zwart Wit",
+    description: "Tijdloze zwart-wit fotografie",
+    // Zet je zwart-wit foto's in images/portfolio/zwartwit/
+    images: [],
+  },
+};
